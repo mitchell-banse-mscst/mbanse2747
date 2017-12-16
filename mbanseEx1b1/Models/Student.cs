6a31944d8a335fd4d77af68a5be29c15530b421a
mitch-banse-mscst <mf2861ys@my.southeastmn.edu>
2017-12-16 13:56:10 -0600

    Add project files.

---
+++ mbanseEx1b1/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mbanseEx1b1.Models
+{
+    public class Student
+    {
+        private string lastName;
+        public int ID { get; set; }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        // public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
